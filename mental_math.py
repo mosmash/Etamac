d3e85df265ec806e5fbc363523d00b974dce0538
Monash Sapkota <monash.sapkota@gmail.com>
2023-12-25 23:18:07 +1100

    app improvements

--- mental_math.py
+++ mental_math.py
@@ -1,9 +1,12 @@
 from tkinter import *
+from tkinter import ttk
 import tkinter as tk
 import random
 from tkinter import messagebox
 from datetime import datetime
 import os
+import time as time_module
+import json
 
 
 def get_datetime():
@@ -22,24 +25,36 @@ class SelectionPage(tk.Tk):
         mainframe.rowconfigure(0, weight=1)
         mainframe.pack(pady=100, padx=100)
         tkvar = StringVar(self)
-        choices = {'add_sub', 'add', 'sub', 'Hard1DigSub' }
+        choices = {'add_sub', 'add', 'sub', 'Hard1DigSub', 'all', 'div3digit'}
         Label(mainframe, text="Choose a test").grid(row=1, column=1)
+        Label(mainframe, text="Enter test time").grid(row=3, column=1)
+        time_entry = Entry(mainframe)
+        time_entry.grid(row=4, column=1)
         popupMenu = OptionMenu(mainframe, tkvar, *choices)
         popupMenu.config(width=20)
 
         popupMenu.grid(row=2, column=1)
 
+        # Add the "Display Logs" button
+        self.display_logs_button = Button(mainframe, text="Display Logs", command=self.display_logs)
+        self.display_logs_button.grid(row=5, column=1)
+
+
 
         def start_practice():
             print(tkvar.get())
             time = 120
+            if time_entry.get():
+                time = int(time_entry.get())  # get the test time from the entry widget
 
             tol = 0.1 # approximation error tol
             root = Tk()
             choices_dict = {'add_sub': ['add', 'sub'],
                             'add': ['add'],
                             'Hard1DigSub': ['Hard1DigSub'],
-                            'sub': ['sub']}
+                            'sub': ['sub'],
+                            'all': ['add', 'sub', 'mul2digit', 'div3digit'],
+                            'div3digit': ['div3digit'],}
 
             app = App(root, time, tol, choices_dict[tkvar.get()])
             root.mainloop()
@@ -47,7 +62,20 @@ class SelectionPage(tk.Tk):
         MyButton1 = Button(mainframe, text="Submit", width=10, command=start_practice)
         MyButton1.grid(row=3, column=1)
 
-
+    def display_logs(self):
+        with open("logs.json", "r") as f:
+            logs_data = json.load(f)
+        # Create a new tkinter window
+        window = tk.Toplevel(self)
+        # Create a treeview in the new window
+        treeview = ttk.Treeview(window, columns=list(logs_data), show="headings")
+        # Set the column identifiers and headings
+        for column in logs_data.keys():
+            treeview.heading(column, text=column)
+        # Insert a new item for each log
+        for log in logs_data:
+            treeview.insert("", "end", values=list(log.values()))
+        treeview.pack(fill="both", expand=True)
 
 class App(tk.Frame):
     def __init__(self, parent, time, tol, choices):
@@ -62,8 +90,8 @@ class App(tk.Frame):
         self.choices = choices
 
         self.parent = parent
-
-
+        self.start_time = time_module.time()
+        self.logs = []
 
         self.parent.title("MENTAL MATH QUANT GRIND")
         self.parent.geometry("700x130")
@@ -78,8 +106,7 @@ class App(tk.Frame):
         self.timer = Label(self, text="Seconds left: 1")
         self.score = Label(self, text="score: 0")
 
-        motive = "[Chorus] Just like Citadel, Jane Street, Two Sigma, Akuna" \
-                 "\nAll I need, yeah, you're all I need."
+        motive = "hi"
 
         self.motive = Label(self, text=motive, font=("Helvetica", 10, "italic"))
 
@@ -101,6 +128,23 @@ class App(tk.Frame):
         os.system("say -v vicki \"" + prompt + "\" -r 120 &") # & to prevent blocking thread
 
     def next_question(self):
+        # Multiplication of 2 digit number by 1-9
+        def mul2digit():
+            x = random.randint(10, 99)
+            y = random.randint(2, 9)
+            prompt = '{} x {} ='.format(x, y)
+            self.ans = x * y
+            return prompt
+        # Divison of 3 digit number by 1-9
+        def div3digit():
+            # make sur number is divisible by y
+            x = random.randint(100, 999)
+            y = random.randint(2, 9)
+            x = x - x % y
+            prompt = '{} / {} ='.format(x, y)
+            self.ans = x / y
+            return prompt
+
         def mul9():
             x = random.randint(1, 9)
             prompt = '{} x 9 ='.format(x)
@@ -194,14 +238,16 @@ class App(tk.Frame):
         return random.choice(self.tests)()
 
     def check_answer(self, event):
-
         user_ans = float(self.answer.get().strip())
         diff = abs(user_ans - self.ans)
         print('diff:', diff, '|exact?', self.exact, '| tol:', self.tol, '| my ans:', user_ans)
 
         if diff == 0 or (not self.exact and diff < self.tol):
-            self.exact = True
+            time_spent = time_module.time() - self.start_time
+            log = {"Question": self.prompt["text"], "Answer": self.ans, "Time spent": time_spent }
+            self.logs.append(log)
             self.correct_ans["text"] = self.prompt["text"] + ('%.3f'% self.ans)
+            self.start_time = time_module.time()
             prompt = self.next_question()
             self.prompt["text"] = prompt
             #self.speak(prompt)
@@ -219,14 +265,37 @@ class App(tk.Frame):
             messagebox.showinfo("TIME'S UP!", "YOUR SCORE = {}".format(self.scr))
             # Append result to log
             datetime = get_datetime()
-            log_score = "grind_sheet.txt"
-            log = datetime + " | " + "score = " + str(self.scr) + " | " + "time = " \
-                  + str(self.time) + " | " + "tests = " + " ;".join([fn.__name__ for fn in self.tests]) \
-                  + "\n"
-            print("Result >>", log)
-            with open(log_score, "a") as f:
-                f.write(log)
-            self.parent.destroy()
+            # Sort logs by time spent
+            self.logs = sorted(self.logs, key=lambda x: x["Time spent"], reverse=True)
+            test_info = {
+                "date": datetime,
+                "score": self.scr,
+                "time": self.time,
+                "tests": [fn.__name__ for fn in self.tests],
+                "logs": self.logs
+            }
+             # Create a new tkinter window
+            window = tk.Toplevel(self.parent)
+            # Create a treeview in the new window
+            treeview = ttk.Treeview(window, columns=list(test_info["logs"][0].keys()), show="headings")
+            # Set the column identifiers and headings
+            for column in test_info["logs"][0].keys():
+                treeview.heading(column, text=column)
+            # Insert a new item for each log
+            for log in test_info["logs"]:
+                treeview.insert("", "end", values=list(log.values()))
+            treeview.pack()
+            if not os.path.isfile("logs.json"):
+                logs_data = []
+            else:
+                # If file exists, load the existing data
+                with open("logs.json", "r") as f:
+                    logs_data = json.load(f)
+            # Append the new data
+            logs_data.append(test_info)
+            # Write the data back to the file
+            with open("logs.json", "w") as f:
+                json.dump(logs_data, f)
         else:
             self.timer['text'] = "Seconds left: " + str(self.count)
             # schedule timer to call myself after 1 second
@@ -256,4 +325,5 @@ if __name__ == '__main__':
 
     app = SelectionPage()
     app.mainloop()
+
 
